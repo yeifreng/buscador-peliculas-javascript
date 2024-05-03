81bdfe986e03c26e00422a261e46e2a42d16ce12
Yeifreng <yeifrenjose@gmail.com>
2024-05-02 21:39:07 -0500

    creando busqueda dinamica en archivo javascript

--- script.js
+++ script.js
@@ -3,13 +3,55 @@ document.getElementById('searchButton').addEventListener('click', searchMovies)
 
 let apiKey = '047661fd4f945020de274f7f9cceb56b';
 let urlBase = 'https://api.themoviedb.org/3/search/movie'
-let pelicula = 'titanic'
-// ?query=Jack+Reacher&api_key=047661fd4f945020de274f7f9cceb56b
+let urlImage = 'https://image.tmdb.org/t/p/w200'
+
+let resultContainer = document.getElementById('results')
 
 function searchMovies(){
+
+    
+    resultContainer.innerHTML = 'Cargando...'
     let searchInput = document.getElementById('searchInput').value
 
-    fetch(`${urlBase}?query=${pelicula}&api_key=${apiKey}`)
+    fetch(`${urlBase}?query=${searchInput}&api_key=${apiKey}`)
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => displayMovies(data.results))
+}
+
+function displayMovies(movies){
+
+    
+    resultContainer.innerHTML = ''
+
+    if(movies.length === 0){
+        resultContainer.innerHTML = '<p>No se econtraron resultados para tu busqueda</p>'
+        return
+    }
+    for(movie of movies){
+       let movieDiv = document.createElement('div')
+       movieDiv.classList.add('movie')
+
+       let title = document.createElement('h2')
+       title.textContent = movie.title
+
+       let releaseDate = document.createElement('p')
+       releaseDate.textContent = `La fecha del lanzamiento fue: ${movie.release_date}`
+
+       let overview = document.createElement('p')
+       overview.textContent = movie.overview
+
+       let posterPath = urlImage + movie.poster_path
+       let poster = document.createElement('img')
+       poster.src = posterPath
+
+
+       movieDiv.appendChild(poster)
+       movieDiv.appendChild(title)
+       movieDiv.appendChild(releaseDate)
+       movieDiv.appendChild(overview)
+
+
+       resultContainer.appendChild(movieDiv)
+
+    }
 }
